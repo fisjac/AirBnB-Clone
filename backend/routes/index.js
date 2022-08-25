c4d6a21cc95d709b8f4cdd2e8a938d79b3599291
fisjac <jfisher9882@gmail.com>
2022-08-25 15:15:00 -0500

    phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
-
-// ...
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -16,5 +9,9 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
-// ...
+
 module.exports = router;
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
