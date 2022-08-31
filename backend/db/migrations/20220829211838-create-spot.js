ebf42e14c7a03e05328cb631743d6351df1f87e5
fisjac <jfisher9882@gmail.com>
2022-08-31 17:26:45 -0500

    removed uniqueness from addresse

--- backend/db/migrations/20220829211838-create-spot.js
+++ backend/db/migrations/20220829211838-create-spot.js
@@ -16,7 +16,7 @@ module.exports = {
       address: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique: true
+        // unique: true
       },
       city: {
         type: Sequelize.STRING,
