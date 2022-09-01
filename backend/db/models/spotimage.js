f90511e875cd3ad33a92d87de3c0e0716bbd28f5
fisjac <jfisher9882@gmail.com>
2022-09-01 12:38:11 -0500

    fixing unseed bug in spot-image

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -11,7 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      SpotImage.belongsTo(models.Spot, {foreignKey: 'spotId'})
+      SpotImage.belongsTo(models.Spot, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE'
+      })
     }
   }
   SpotImage.init({
@@ -27,6 +30,7 @@ module.exports = (sequelize, DataTypes) => {
     preview: {
       type: DataTypes.BOOLEAN,
       // allowNull: false
+         defaultValue: false
     }
   }, {
     sequelize,
