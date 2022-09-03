fc738b1bcb756edb9f865f2ea335d6264a526b74
fisjac <jfisher9882@gmail.com>
2022-09-03 12:25:53 -0500

    get spots by current user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const { requireAuth, restoreUser } = require('../../utils/auth');
-const {User, Spot, Review, sequelize} = require('../../db/models');
+const {User, Spot, Review, Booking, sequelize} = require('../../db/models');
 const { Op } = require('sequelize');
 const helperFuncs = require('../../utils/helperFuncs')
 const customValidators = require('../../utils/validation')
 
 
 
-// router.get('/current',
+router.get('/current',
+  restoreUser,
+  requireAuth,
+  async (req, res, next) => {
+    let bookings = await Booking.findAll({
+      include: [
+        {
+          model: Spot
+        },
+      ]
+    });
 
-// );
+    for (let booking of bookings) {
+      booking.dataValues.Spot.dataValues.previewImage = await helperFuncs.getPreviewForSpot(booking.dataValues.Spot);
+    };
+
+    res.status = 200;
+    res.json(bookings);
+  }
+);
 
 
 
