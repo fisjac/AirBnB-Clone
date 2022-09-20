ad6b90b2792e51091b8d4deedbfc22064c051bab
fisjac <jfisher9882@gmail.com>
2022-09-20 13:29:56 -0500

    finished seeding spot images

--- backend/db/seeders/20220830213547-seed-spotImages.js
+++ backend/db/seeders/20220830213547-seed-spotImages.js
@@ -28,52 +28,52 @@ const spotImagesSeed = [
   },
   {
     spotId: 6,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2017/11/16/19/29/cottage-2955582__340.jpg',
     preview: true
   },
   {
     spotId: 7,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2017/09/17/18/15/lost-places-2759275__340.jpg',
     preview: true
   },
   {
     spotId: 8,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2017/07/09/03/19/home-2486092__340.jpg',
     preview: true
   },
   {
     spotId: 9,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2013/11/13/21/14/san-francisco-210230__340.jpg',
     preview: true
   },
   {
     spotId: 10,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221__340.jpg',
     preview: true
   },
   {
     spotId: 11,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2021/11/08/00/30/bedroom-6778193__340.jpg',
     preview: true
   },
   {
     spotId: 12,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2017/01/07/17/48/interior-1961070__340.jpg',
     preview: true
   },
   {
     spotId: 13,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2016/04/18/08/50/kitchen-1336160__340.jpg',
     preview: true
   },
   {
     spotId: 14,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2017/07/09/03/19/home-2486092__340.jpg',
     preview: true
   },
   {
     spotId: 15,
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271__340.jpg',
+    url: 'https://cdn.pixabay.com/photo/2016/11/22/19/11/brick-wall-1850095__340.jpg',
     preview: true
   },
 ]
