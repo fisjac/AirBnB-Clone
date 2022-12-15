836f5f788dbc53f5d6884c0794dea279ab02ddf7
fisjac <jfisher9882@gmail.com>
2022-12-15 15:42:05 -0600

    bookings reducer - first pass

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -1,10 +1,34 @@
 import { csrfFetch } from "./csrf";
 
-const ADD_SPOT_BOOKINGS = 'bookings/set';
+const LOAD_SPOT_BOOKINGS = 'bookings/set';
 
-export const addBookins = (bookings) => {
+export const loadSpotBookings = ({bookings}) => {
   return {
-    type: ADD_SPOT_BOOKINGS,
-    payload: bookings
+    type: LOAD_SPOT_BOOKINGS,
+    bookings
+  }
+};
+
+export const getSpotBookings = (spotId) => async dispatch => {
+  const response = await csrfFetch(`/api/spots/${spotId}/bookings`);
+  if (response.ok) {
+    const bookings = await response.json();
+     dispatch(addBookings(bookings));
+  };
+  return response
+};
+
+let initialSate = {
+  spotBookings: null,
+  userBookings: null,
+};
+
+let newState;
+export default function bookingsReducer (state = initialState, action) {
+  switch (action.type) {
+    case LOAD_SPOT_BOOKINGS:
+      return {spotBookings: [...action.bookings]}
+    default:
+      return {...state}
   }
 };
